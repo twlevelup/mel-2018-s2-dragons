ca072762860dd94418355ddd78761a7c4d85785e
Lam <lamtu1008@gmail.com>
2018-08-28 20:38:05 +1000

    [Lam/Bernado] Add placeholders for 2 options

--- client/spec/pages/gamePage.spec.js
+++ client/spec/pages/gamePage.spec.js
@@ -1,15 +1,20 @@
-const GamePage = require('../../src/js/pages/gamePage');
+const GamePage = require("../../src/js/pages/gamePage");
 
 describe("gamePage", () => {
   let watchFace;
   beforeEach(() => {
     document.body.innerHTML = `<div id='watch-face' style='height: 100px; width: 100px;'></div>`;
-    watchFace = document.getElementById('watch-face');
+    watchFace = document.getElementById("watch-face");
   });
 
-  it('should have a template', () => {
-    const page = new GamePage(); 
-    expect(page.template()).toContain("<h1>Game Page</h1>")
-  })  
+  it("should have a template", () => {
+    const page = new GamePage();
+    expect(page.template()).toContain("<h1>Game Page</h1>");
+  });
 
-})
+  it("should have two options", () => {
+    const page = new GamePage();
+    expect(page.template()).toContain("<p>Option1</p>");
+    expect(page.template()).toContain("<p>Option2</p>");
+  });
+});
